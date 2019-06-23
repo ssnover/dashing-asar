88a14eb94e3bc7468b6d6b6c6d47903562430696
ssnover95 <sws7379@rit.edu>
2019-06-23 14:58:05 -0600

    Achieve image transport from the publisher under ros2!

--- image_capture/src/run_publish_images.cpp
+++ image_capture/src/run_publish_images.cpp
@@ -4,8 +4,13 @@
  */
 
 #include <chrono>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include "cv_bridge/cv_bridge.h"
+#include "image_transport/image_transport.h"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+
 
 using namespace std::chrono_literals;
 
@@ -13,31 +18,50 @@ class ImagePublisher : public rclcpp::Node
 {
 public:
 
-  ImagePublisher(std::string const & filepath)
+  explicit ImagePublisher(std::string const & filepath)
     : Node("image_publisher"),
       mFilepath(filepath),
-      mPublisher(create_publisher<std_msgs::msg::String>("/image_filepath", 5)),
-      mTimer(create_wall_timer(1s, std::bind(&ImagePublisher::publishStringCallback, this)))
+      mImage(cv_bridge::CvImage(std_msgs::msg::Header(),
+                                "bgr8",
+                                cv::imread(mFilepath,
+                                           cv::IMREAD_COLOR)).toImageMsg())
   {
+  }
+
+  void start()
+  {
+    image_transport::ImageTransport it(shared_from_this());
+    mPublisher = it.advertise("asar_image", 1);
+    mTimer = create_wall_timer(1s, std::bind(&ImagePublisher::publishCallback, this));
   }
 
 private:
 
-  void publishStringCallback()
+  void publishCallback()
   {
-    auto message = std_msgs::msg::String();
-    message.data = mFilepath;
-    mPublisher->publish(message);
+    mPublisher.publish(mImage);
   }
 
   std::string mFilepath;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr mPublisher;
+  sensor_msgs::msg::Image::SharedPtr mImage;
+  image_transport::Publisher mPublisher;
   rclcpp::TimerBase::SharedPtr mTimer;
 };
 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<ImagePublisher>("hello world"));
+  std::string const FILENAME{"/home/ssnover/asar2.jpg"};
+
+  auto image = cv::imread(FILENAME, cv::IMREAD_COLOR);
+  cv::namedWindow("Test Image", cv::WINDOW_NORMAL);
+  cv::imshow("Test Image", image);
+  cv::waitKey(0);
+  cv::destroyAllWindows();
+
+
+  auto node = std::make_shared<ImagePublisher>(FILENAME);
+  node->start();
+  rclcpp::spin(node);
   rclcpp::shutdown();
 }
